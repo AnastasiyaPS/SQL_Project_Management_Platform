59562b0be810cd3a596747c1dbc573aa94b01700
Nastya <129861346+AnastasiyaPS@users.noreply.github.com>
2026-01-28 15:16:35 +0300

    Update Script_index.sql

--- Scripts/Script_index.sql
+++ Scripts/Script_index.sql
@@ -3,20 +3,21 @@
  * due_date часто используется для сортировки/фильтрации * 
  *   */
 -- индекс для поиска задач (по проекту)
-create index ind_task_project_id on task(project_id)
+create index ind_task_project_id on task(project_id);
 
 --индекс для поиска задач (по исполнителю)
-create index ind_task_assigned_user on task(assigned_user_id)
+create index ind_task_assigned_user on task(assigned_user_id);
 
 --для анализа статуса задач scd
-create index ind_task_status_history_task  on task_status_history(task_id, is_current)
+create index ind_task_status_history_task  on task_status_history(task_id, is_current);
 
 --для фильтрации по статусу 
-create index ind_task_current_status on task(current_status_id)
+create index ind_task_current_status on task(current_status_id);
 
 --для анализа задач по строкамм
-create index ind_task_due_date  on task(due_date)
+create index ind_task_due_date  on task(due_date);
 
 --уникальный индекс для текущего статуса задачи
 create unique index uq_task_status
-on task_status_history(task_id) where is_current = true
+on task_status_history(task_id) where is_current = true;
+
