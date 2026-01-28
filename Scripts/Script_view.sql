9f42690a40a0986e7f06d2b7e888635489553ed5
Nastya <129861346+AnastasiyaPS@users.noreply.github.com>
2026-01-28 15:20:57 +0300

    Update Script_view.sql

--- Scripts/Script_view.sql
+++ Scripts/Script_view.sql
@@ -11,9 +11,9 @@ select
 from task t
 join project p on p.project_id = t.project_id
 left join users u on u.user_id = t.assigned_user_id
-join status s on s.status_id = t.current_status_id
+join status s on s.status_id = t.current_status_id;
 
-SELECT * FROM v_current_task
+SELECT * FROM v_current_task;
 
 
 --просроченные задачи 
@@ -22,7 +22,7 @@ select *
 from task 
 where due_date < current_date and current_status_id <> (
 select status_id from status  
-where status_name='Завершена')
+where status_name='Завершена');
 
 --история статусов задач
 create view v_task_full_history as select
@@ -33,4 +33,5 @@ tsh.valid_from,
 tsh.valid_to
 from task_status_history tsh
 join task t on t.task_id=tsh.task_id
-join status s on s.status_id=tsh.status_id
+join status s on s.status_id=tsh.status_id;
+
