2876f97e1d12cbb2126010917f495e6152337a5f
Nastya <129861346+AnastasiyaPS@users.noreply.github.com>
2026-01-28 15:16:11 +0300

    Update Script_check.sql

--- Scripts/Script_check.sql
+++ Scripts/Script_check.sql
@@ -1,13 +1,14 @@
 --проверки и ограничения 
 alter table task  add constraint chk_task_dse_date
-check (due_date>=created_at)
+check (due_date>=created_at);
 
 --проверка почты пользователя 
 alter table users  add constraint chk_user_email
-check (position('@' in email)>1)
+check (position('@' in email)>1);
 
 --проверка дат 
 alter table project  add constraint chk_project_date
-check (end_date is null or end_date>=start_date)
+check (end_date is null or end_date>=start_date);
+
 
 
